f821276c8441d611c16872923247a7e1aa5db116
ryosantouchh <kittitat.suntimak@gmail.com>
2023-03-10 19:17:06 +0700

    complete all tasks

---
+++ challenge1.js
@@ -0,0 +1,5 @@
+const birthDay = 1995;
+const futureDay = 2037;
+
+const diffYear = futureDay - birthDay;
+console.log(diffYear);
---
+++ challenge2.js
@@ -0,0 +1,9 @@
+let person = {
+  name: "Kittitat",
+  location: "Chonburi",
+  hobby: "playing music",
+};
+
+console.log(
+  `Hi, my name is ${person.name}. I live in ${person.location} and enjoy ${person.hobby}.`
+);
